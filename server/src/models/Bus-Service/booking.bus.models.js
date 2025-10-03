1285892d934c5e88ac6e79aac2a802f45840ada4
jainAmishhh <jainamish1511@gmail.com>
2025-10-03 09:12:18 +0530

    Add booking model for bus service

---
+++ server/src/models/Bus-Service/booking.bus.models.js
@@ -0,0 +1,95 @@
+import mongoose from "mongoose";
+ 
+const bookingSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    trip: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "BusTrip",
+      required: true,
+      index: true,
+    },
+    providerBookingId: {
+      type: String,
+      index: true,
+      unique: true,
+      sparse: true,
+    },
+    seats: [
+      {
+        seatNumber: Number,
+        seatID: String,
+        fare: Number, // store actual paid fare per seat
+      },
+    ],
+    boardingPoint: {
+      location: {
+        type: String,
+        required: true,
+      },
+      time: {
+        type: Date,
+        required: true,
+      },
+      landmark: {
+        type: String,
+      },
+    },
+    droppingPoint: {
+      location: {
+        type: String,
+        required: true,
+      },
+      time: {
+        type: Date,
+      },
+      landmark: {
+        type: String,
+      },
+    },
+    status: {
+      type: String,
+      enum: ["initiated", "confirmed", "cancelled", "failed"],
+      default: "initiated",
+    },
+    bookedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    ticketURL: {
+      type: String, // PDF/e-ticket link
+    },
+    totalFare: {
+      finalFare: {
+        type: Number,
+        default: 0,
+      },
+      discount: {
+        type: Number,
+        default: 0,
+      },
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "paid", "failed"],
+      default: "pending",
+    },
+    latestPayment: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "Payment" 
+    },
+    cancellation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cancellation", // references separate cancellation collection
+    },
+  },
+  { timestamps: true }
+);
+
+bookingSchema.index({ user: 1, trip: 1, providerBookingId: 1 });
+
+export default mongoose.model("Booking", bookingSchema);
