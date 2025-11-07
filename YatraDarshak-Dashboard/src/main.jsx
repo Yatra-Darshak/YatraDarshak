7cdb7abf5f0134eec58b83708150ee122cb26855
jainAmishhh <jainamish1511@gmail.com>
2025-11-07 19:42:04 +0530

    Update main entry point for React rendering with routing context

--- YatraDarshak-Dashboard/src/main.jsx
+++ YatraDarshak-Dashboard/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const clientId = "584220536529-c6c1ku3bc0am8ekjpk3l8hfhv9247ac9.apps.googleusercontent.com";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
